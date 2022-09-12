c44f0e0e9097c54e0fdd039f8ce9027ef5c36959
hakareem <harithakareem72@gmail.com>
2022-09-12 15:20:18 +0100

    game class

---
+++ game/app/src/main/java/game/Game.java
@@ -0,0 +1,11 @@
+package game;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public String getWordToGuess(String word) {
+        return word;
+    }
+}
