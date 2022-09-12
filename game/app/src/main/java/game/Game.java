60a2b8a7e8debf5fa84a21043a033a433632f058
hakareem <harithakareem72@gmail.com>
2022-09-12 16:19:02 +0100

    added string builder

--- game/app/src/main/java/game/Game.java
+++ game/app/src/main/java/game/Game.java
@@ -1,11 +1,17 @@
 package game;
 
 public class Game {
+    // constructor 
+    public Game(String word) {}
     public static void main(String[] args) {
         System.out.println();
     }
 
     public String getWordToGuess(String word) {
-        return word;
+        StringBuilder strBuilder = new StringBuilder(word);
+        for (int i = 1; i < word.length(); i++){
+            strBuilder.replace(i, word.length(), "_");
+        }
+        return strBuilder.toString();
     }
 }
