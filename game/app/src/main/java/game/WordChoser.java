78faf820cc6e2091da811bf3bbdac7e390656048
hakareem <harithakareem72@gmail.com>
2022-09-13 16:12:42 +0100

    word dictionary

--- game/app/src/main/java/game/WordChoser.java
+++ game/app/src/main/java/game/WordChoser.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class WordChoser {
   public static final String[] DICTIONARY = {"PENALDO", "PENZEMA", "PESSI", "LAKAKA"};
   
+  // selects a random word from the dictionary
   public String getRandomWordFromDictionary() {
     Random rand = new Random();
     return DICTIONARY[rand.nextInt(DICTIONARY.length)];
