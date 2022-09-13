79b74be383207c9bff4df65e40df96695a05752f
hakareem <harithakareem72@gmail.com>
2022-09-13 16:56:55 +0100

    new masker class

---
+++ game/app/src/main/java/game/Masker.java
@@ -0,0 +1,26 @@
+package game;
+
+import java.util.ArrayList;
+
+public class Masker {
+
+     // handles the word displayed to user logic 
+    public String getMaskedWord(String word, ArrayList<Character> guessedLetters) {
+        StringBuffer str = new StringBuffer(); // what is the difference between string builder and string buffer?
+
+        for (int i = 0; i < word.length(); i++) {
+            Character currentLetter = word.charAt(i);
+            
+            if (i == 0) { // first char always visible
+                str.append(currentLetter);
+            } else {
+                if (guessedLetters.indexOf(currentLetter) != -1) { // append char if guess is correct
+                    str.append(currentLetter);
+                } else {
+                    str.append("_");
+                }
+            }
+        }
+        return str.toString();
+    }
+}
