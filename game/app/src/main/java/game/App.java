0aae0113c19f59a24f9f5b45b94d99ad6dcb7280
hakareem <harithakareem72@gmail.com>
2022-09-13 16:12:20 +0100

    app interface

--- game/app/src/main/java/game/App.java
+++ game/app/src/main/java/game/App.java
@@ -7,20 +7,18 @@ public class App {
         WordChoser wordchoser = new WordChoser();
         Game game = new Game(wordchoser);
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // creating a scanner object
         System.out.println("Welcome to the word guessing game!");
         System.out.println("This is your word:");
 
-        while (game.isGameWon()==false) {
-            System.out.printf("Guess this word is: %s\n", game.getWordToGuess());
+        while (game.isGameWon()== false) {
+            System.out.printf("Guess this word is: %s\n", game.getWordToGuess()); 
 
             System.out.println("Give me a letter:");
-            // store users guess
-            char guess = scanner.next().charAt(0); 
-            // boolean indicating whether it's right or wrong
-            System.out.println(game.guessLetter(guess)); 
+            char guess = scanner.next().charAt(0); // reading and storing user input
+            System.out.println(game.guessLetter(guess)); // boolean indicating whether it's right or wrong
 
-            // winning and losing conditions   
+            // win and lose conditions   
             if (game.isGameLost()) {
                 System.out.println("omg you're so bad");
                 break;
@@ -30,7 +28,7 @@ public class App {
                 break;
             }
         }
-        scanner.close();
+        scanner.close(); 
     }
 }
 
