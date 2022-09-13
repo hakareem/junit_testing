453729a93cae85e7d7f5e17ae57a7fa0122e78fe
hakareem <harithakareem72@gmail.com>
2022-09-13 08:41:35 +0100

    gradle build

---
+++ debugging/mocked.java
@@ -0,0 +1,17 @@
+/* 
+
+@Test public void testGetsWordToGuessWithRandomWord() {
+    WordChoser mockedChoser = mock(WordChoser.class);
+    when(mockedChoser.getRandomWordFromDictionary()).thenReturn("DEVELOPER");
+
+    Game game = new Game(mockedChoser);
+
+    assertEquals(game.getWordToGuess(), "D________");
+}
+
+
+public Game(WordChoser choser) {
+    this.word = choser.getRandomWordFromDictionary();
+}
+
+*/
